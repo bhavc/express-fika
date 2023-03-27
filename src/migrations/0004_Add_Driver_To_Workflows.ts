3abcd4d8017caff2035b7dbce34f06092f14e146
bhav <bhavdip@gmail.com>
2023-03-26 21:26:53 -0400

    Add migration to add assigned driver to workflow

---
+++ src/migrations/0004_Add_Driver_To_Workflows.ts
@@ -0,0 +1,27 @@
+import { Kysely, sql } from "kysely";
+
+// TODO: move into its own migration during production
+export async function up(db: Kysely<any>): Promise<void> {
+	await db.schema
+		.alterTable("workflow")
+		.addColumn("assigned_driver", "integer")
+		.execute();
+
+	await db.schema
+		.alterTable("workflow")
+		.addForeignKeyConstraint(
+			"assigned_driver_constraint",
+			["assigned_driver"],
+			"users",
+			["id"]
+			// (cb) => cb.onDelete("cascade")
+		)
+		.execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+	await db.schema
+		.alterTable("workflow")
+		.dropColumn("assigned_driver")
+		.execute();
+}
